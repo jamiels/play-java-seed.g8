b1e7becd734281c32cda4f7437839d946f34e619
jamiel <i@jamiel.net>
2017-11-22 21:47:40 -0500

    update to carbon

---
+++ src/main/g8/app/models/SampleEntity.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.List;
+import javax.persistence.Entity;
+import io.ebean.Finder;
+
+
+@Entity
+public class SampleEntity extends BaseModel {
+	
+	public static Finder<Long, SampleEntity> find = new Finder<>(SampleEntity.class);
+	public static List<SampleEntity> getAllCurrent() {
+		return find.query().where().eq("current", true).findList();
+	}
+	
+	public static SampleEntity findByUUID(String uuid) {
+		return find.query().where().eq("uuid", uuid).findUnique();
+	}
+}
